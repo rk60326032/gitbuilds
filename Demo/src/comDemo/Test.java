bc252d107c2f0e81649b4daa8f8d21ecf81a94f9
Rohit <rk60326032@gmail.com>
2021-08-17 11:07:39 +0530

    Intial commit by Rohit

---
+++ Demo/src/comDemo/Test.java
@@ -0,0 +1,10 @@
+package comDemo;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi, Welcome to Devops world 1 !");
+	}
+
+}
