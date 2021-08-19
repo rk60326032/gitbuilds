2f390ccbce24277059adfc5205884b2371a5d017
Rohit <rk60326032@gmail.com>
2021-08-19 09:27:22 +0530

    command line arg commit

--- Demo/src/comDemo/Test.java
+++ Demo/src/comDemo/Test.java
@@ -4,7 +4,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi, Welcome to Devops world 12345 !");
+		//System.out.println("Hi, Welcome to Devops world 12345 !");
+		System.out.println("Your first argument is: "+args[0]);
+		
 	}
 
 }
